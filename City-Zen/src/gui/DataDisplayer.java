1e0f3f840fde63d7c831694d51604628621fea19
ToukaM <t_merouani@outlook.fr>
2019-02-15 15:00:53 +0100

    DataDisplayer HBox added

---
+++ City-Zen/src/gui/DataDisplayer.java
@@ -0,0 +1,24 @@
+package gui;
+
+import gui_data.BlockSize;
+import javafx.scene.layout.HBox;
+
+public class DataDisplayer extends HBox {
+	
+	private BlockSize blockSize;
+
+	public DataDisplayer(double width, double height) {
+		super();
+		setBlockSize(new BlockSize(width, height));
+		setPrefSize(getBlockSize().getWidth(), getBlockSize().getHeight());
+	}
+	
+	public BlockSize getBlockSize() {
+		return blockSize;
+	}
+
+	public void setBlockSize(BlockSize blockSize) {
+		this.blockSize = blockSize;
+	}
+	
+}
