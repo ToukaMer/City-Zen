165b137bc6210c636cc2e2d21642579e266780b0
phanoro <srfunkyman95@outlook.fr>
2019-02-15 19:17:22 +0100

    parameter root added to Toolbar, menu button added to root

--- City-Zen/src/gui/Toolbar.java
+++ City-Zen/src/gui/Toolbar.java
@@ -1,16 +1,24 @@
 package gui;
 
 import gui_data.BlockSize;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 
 public class Toolbar extends HBox {
 
 	private BlockSize blockSize;
+	private Button menu;
+	private Root root;
 
-	public Toolbar(double width, double height) {
+	public Toolbar(double width, double height, Root root) {
 		super();
 		setBlockSize(new BlockSize(width, height));
 		setPrefSize(getBlockSize().getWidth(), getBlockSize().getHeight());
+		setRoot(root);
+		initMenu();
+		getChildren().add(getMenu());
 	}
 
 	public BlockSize getBlockSize() {
@@ -19,5 +27,30 @@ public class Toolbar extends HBox {
 
 	public void setBlockSize(BlockSize blockSize) {
 		this.blockSize = blockSize;
+	}
+	
+	public void initMenu(){
+		setMenu(new Button("Menu"));
+		this.menu.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                root.getPause().toggle();
+            }
+        });
+	}
+
+	public Button getMenu() {
+		return menu;
+	}
+
+	public void setMenu(Button menu) {
+		this.menu = menu;
+	}
+
+	public Root getRoot() {
+		return root;
+	}
+
+	public void setRoot(Root root) {
+		this.root = root;
 	}
 }
