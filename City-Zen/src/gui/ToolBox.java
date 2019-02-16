eec13d94fd8260dee890cd3dd30ffb3aab7546e8
Ludovic Domingues <ludovic.domingues96@gmail.com>
2019-02-16 19:18:24 +0100

    Resolving conflict with toolbox class

--- City-Zen/src/gui/ToolBox.java
+++ City-Zen/src/gui/ToolBox.java
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-package gui;
-
-import gui_data.BlockSize;
-import javafx.scene.layout.HBox;
-
-public class ToolBox extends HBox{
-	
-	private BlockSize blockSize;
-	
-	public ToolBox(double width, double height) {
-		super();
-		setBlockSize(new BlockSize(width, height));
-		setPrefSize(getBlockSize().getWidth(), getBlockSize().getHeight());
-	}
-	
-	public BlockSize getBlockSize() {
-		return blockSize;
-	}
-
-	public void setBlockSize(BlockSize blockSize) {
-		this.blockSize = blockSize;
-	}
-}
-=======
 package gui;
 
 import gui_data.BlockSize;
@@ -67,4 +42,3 @@ public class ToolBox extends HBox{
 		this.blockSize = blockSize;
 	}
 }
->>>>>>> c9fe30f116e90a93a62b2e44e320527a9ba1be97
