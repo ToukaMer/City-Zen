28fc28cb662267d9065c0ec9f3ee88d02c000411
Ludovic Domingues <ludovic.domingues96@gmail.com>
2019-02-22 15:15:40 +0100

    Forgot to add file to commit...

---
+++ City-Zen/src/data/Constants.java
@@ -0,0 +1,20 @@
+package data;
+
+public class Constants {
+	public static int RESIDENCE = 2;
+	public static int COMMERCIAL = 3;
+	public static int ADMINISTRATIVE = 1;
+	public static int WILDERNESS = 0;
+	
+	public static int RAILWAY = 1;
+	public static int STATION = 2;
+	public static int WILDERNESSRR = 0;
+	
+	
+	public Constants() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
