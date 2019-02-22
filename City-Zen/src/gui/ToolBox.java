44f55f3a3a8ed0076795c08583cd41d40cc1975a
ToukaM <t_merouani@outlook.fr>
2019-02-22 16:45:44 +0100

    Tooltip problem resolved

--- City-Zen/src/gui/ToolBox.java
+++ City-Zen/src/gui/ToolBox.java
@@ -4,9 +4,10 @@ import gui_data.BlockSize;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.TilePane;
 
-public class ToolBox extends TilePane{
+public class ToolBox extends VBox{
 	
 	private BlockSize blockSize;
 	private Label lbl_toolBox;
@@ -16,28 +17,48 @@ public class ToolBox extends TilePane{
 	private Button destroyResidence;
 	private Button destroyCommercial;
 	private Button destroyAdministrative;
+	private Tooltip BRTooltip = new Tooltip();
+	private Tooltip BCTooltip = new Tooltip();
+	private Tooltip BATooltip = new Tooltip();
+	private Tooltip DRTooltip = new Tooltip();
+	private Tooltip DCTooltip = new Tooltip();
+	private Tooltip DATooltip = new Tooltip();
 	
 	public ToolBox(double width, double height) {
 		super();
 		setBlockSize(new BlockSize(width, height));
-		setHgap(20);
-		setVgap(20);
 		setPrefSize(getBlockSize().getWidth(), getBlockSize().getHeight());
-		//lbl_toolBox = new Label("TB");
+		lbl_toolBox = new Label("TB");
 		buildResidence = new Button("BR");
-		buildResidence.setTooltip(new Tooltip("Build a residance"));
+		BRTooltip.setText("Build a residence");
+		buildResidence.setTooltip(BRTooltip);
 		buildCommercial = new Button("BC");
 		buildAdministrative = new Button("BA");
 		destroyResidence = new Button("DR");
 		destroyCommercial = new Button("DC");
 		destroyAdministrative = new Button("DA");
-		//getChildren().add(lbl_toolBox);
-		getChildren().add(buildAdministrative);
-		getChildren().add(buildCommercial);
-		getChildren().add(buildResidence);
-		getChildren().add(destroyResidence);
-		getChildren().add(destroyCommercial);
-		getChildren().add(destroyAdministrative);
+		BCTooltip.setText("Build a commercial");
+		buildCommercial.setTooltip(BCTooltip);
+		BATooltip.setText("Build an administrative");
+		buildAdministrative.setTooltip(BATooltip);
+		DRTooltip.setText("Destroy a residence");
+		destroyResidence.setTooltip(DRTooltip);
+		DCTooltip.setText("Destroy a commercial");
+		destroyCommercial.setTooltip(DCTooltip);
+		DATooltip.setText("Destroy an administrative");
+		destroyAdministrative.setTooltip(DATooltip);
+		getChildren().add(lbl_toolBox);
+		TilePane tilePane = new TilePane();
+		tilePane.setHgap(20);
+		tilePane.setVgap(20);
+		tilePane.getChildren().add(buildAdministrative);
+		tilePane.getChildren().add(buildCommercial);
+		tilePane.getChildren().add(buildResidence);
+		tilePane.getChildren().add(destroyResidence);
+		tilePane.getChildren().add(destroyCommercial);
+		tilePane.getChildren().add(destroyAdministrative);
+		
+		getChildren().add(tilePane);
 		
 	}
 	
