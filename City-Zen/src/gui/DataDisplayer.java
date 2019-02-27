ae802aa492d01e43e93a31cd0d318bc943526991
ToukaM <t_merouani@outlook.fr>
2019-02-28 00:46:06 +0100

    Labels added to DataDisplayer

--- City-Zen/src/gui/DataDisplayer.java
+++ City-Zen/src/gui/DataDisplayer.java
@@ -1,5 +1,6 @@
 package gui;
 
+import data.Stats;
 import gui_data.BlockSize;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -7,17 +8,68 @@ import javafx.scene.layout.VBox;
 public class DataDisplayer extends VBox {
 	
 	private BlockSize blockSize;
-	private Label lbl_dataDisplayer;
+	private Label titleLabel;
+	private Label moneyLabel;
+	private Label nbResidentialLabel;
+	private Label nbCommercialLabel;
+	private Label nbAdministrativeLabel;
+	private Label nbStationLabel;
 	
-
 	public DataDisplayer(double width, double height) {
 		super();
 		setBlockSize(new BlockSize(width, height));
 		setPrefSize(getBlockSize().getWidth(), getBlockSize().getHeight());
 		
-		lbl_dataDisplayer = new Label("Data");
+		initializeLabels();
 		
-		getChildren().add(lbl_dataDisplayer);
+		getChildren().add(getTitleLabel());
+		getChildren().add(getMoneyLabel());
+		getChildren().add(getNbResidentialLabel());
+		getChildren().add(getNbCommercialLabel());
+		getChildren().add(getNbAdministrativeLabel());
+	}
+	
+	public void initializeLabels() {
+		initializeTitleLabel();
+		initializeMoneyLabel();
+		initializeNbResidentialLabel();
+		initializeNbCommercialLabel();
+		initializeNbAdministrativeLabel();
+	}
+	
+	public void initializeTitleLabel() {
+		setTitleLabel(new Label());
+		getTitleLabel().setText("Data");
+		getTitleLabel().getStyleClass().remove("label");
+		getTitleLabel().getStyleClass().add("toolBoxLabel");
+	}
+	
+	public void initializeMoneyLabel() {
+		setMoneyLabel(new Label());
+		getMoneyLabel().setText("Money :"+Stats.money);
+		getMoneyLabel().getStyleClass().remove("label");
+		getMoneyLabel().getStyleClass().add("dataLabel");
+	}
+	
+	public void initializeNbResidentialLabel() {
+		setNbResidentialLabel(new Label());
+		getNbResidentialLabel().setText("Nb residential :"+Stats.nbResidence);
+		getNbResidentialLabel().getStyleClass().remove("label");
+		getNbResidentialLabel().getStyleClass().add("dataLabel");
+	}
+	
+	public void initializeNbCommercialLabel() {
+		setNbCommercialLabel(new Label());
+		getNbCommercialLabel().setText("Nb commercial :"+Stats.nbCommercial);
+		getNbCommercialLabel().getStyleClass().remove("label");
+		getNbCommercialLabel().getStyleClass().add("dataLabel");
+	}
+	
+	public void initializeNbAdministrativeLabel() {
+		setNbAdministrativeLabel(new Label());
+		getNbAdministrativeLabel().setText("Nb Administrative :"+Stats.nbAdministrative);
+		getNbAdministrativeLabel().getStyleClass().remove("label");
+		getNbAdministrativeLabel().getStyleClass().add("dataLabel");
 	}
 	
 	public BlockSize getBlockSize() {
@@ -27,5 +79,54 @@ public class DataDisplayer extends VBox {
 	public void setBlockSize(BlockSize blockSize) {
 		this.blockSize = blockSize;
 	}
+	
+	public Label getTitleLabel() {
+		return titleLabel;
+	}
+
+	public void setTitleLabel(Label titleLabel) {
+		this.titleLabel = titleLabel;
+	}
+
+	public Label getMoneyLabel() {
+		return moneyLabel;
+	}
+
+	public void setMoneyLabel(Label moneyLabel) {
+		this.moneyLabel = moneyLabel;
+	}
+
+	public Label getNbResidentialLabel() {
+		return nbResidentialLabel;
+	}
+
+	public void setNbResidentialLabel(Label nbResidentialLabel) {
+		this.nbResidentialLabel = nbResidentialLabel;
+	}
+
+	public Label getNbCommercialLabel() {
+		return nbCommercialLabel;
+	}
+
+	public void setNbCommercialLabel(Label nbCommercialLabel) {
+		this.nbCommercialLabel = nbCommercialLabel;
+	}
+
+	public Label getNbAdministrativeLabel() {
+		return nbAdministrativeLabel;
+	}
+
+	public void setNbAdministrativeLabel(Label nbAdministrativeLabel) {
+		this.nbAdministrativeLabel = nbAdministrativeLabel;
+	}
+
+	public Label getNbStationLabel() {
+		return nbStationLabel;
+	}
+
+	public void setNbStationLabel(Label nbStationLabel) {
+		this.nbStationLabel = nbStationLabel;
+	}
+
 	
 }
