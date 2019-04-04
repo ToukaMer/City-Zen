f4b718aed554d5e34e4ee7df52d7ea20056a0551
Kiérian Foubert <Kirian Foubert@10.77.32.254>
2019-04-04 17:06:25 +0200

    1er Comit satisfaction

---
+++ City-Zen/src/engine/managers/SatisfactionManager.java
@@ -0,0 +1,36 @@
+package engine.managers;
+
+import data.Coordinates;
+import data.Stats;
+import data.districtData.District;
+import data.districtData.Residencial;
+import data.railRoadData.NotRailed;
+import data.railRoadData.RailSquare;
+import engine.Game;
+import gui_data.GuiConstants;
+
+public final class SatisfactionManager 
+{
+	public int mathSatisfaction() 
+	{
+	District[][] map = Game.getINSTANCE().getDistrictMap();
+	int satisfaction;
+	satisfaction = 0;
+	RailSquare railSquare[][] = new RailSquare[GuiConstants.SQUARE_PER_COLUMN][GuiConstants.SQUARE_PER_ROW];
+	int column = 0,row = 0;
+		while( column<GuiConstants.SQUARE_PER_ROW) 
+		{
+			while(row<GuiConstants.SQUARE_PER_COLUMN) 
+			{
+				if (map[column][row].getType() == 2)
+				{
+					satisfaction = satisfaction + map[column][row].getSatisfaction()*((Residencial)map[column][row]).getNbHab();
+				}
+				row++;
+			}
+			column++;
+		}
+		satisfaction = satisfaction/Game.getINSTANCE().getStats().nbHab;
+		return satisfaction;
+	}
+}
